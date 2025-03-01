c197bbe10962578fd050bdd0051d5a9dc998f481
TakamiChie@Ardha <chie@onpu-tamago.net>
2025-03-01 19:10:13 +0900

    テストが実行できるようにコードを追加

--- tests/test.py
+++ tests/test.py
@@ -157,19 +157,21 @@ class TestAudio2MP4(unittest.TestCase):
     # テスト用の図と軸の作成
     video_size = (640, 480)
     fig, ax = plt.subplots(figsize=(video_size[0]/100, video_size[1]/100))
-
+    ax.set_facecolor("black")
+    ax.set_xlim(0, video_size[0])
+    ax.set_ylim(0, video_size[1])
 
     # テスト用テキストとフォント名
     sample_text = "これはテストです"
     sample_font = "Meiryo"
 
     # draw_text関数の呼び出し
-    text_obj = draw_text(ax, video_size, sample_text, sample_font)
+    text_obj = draw_text(ax, video_size, sample_text, sample_font, 1)
 
     # テキストオブジェクトの各プロパティの検証
     self.assertIsNotNone(text_obj)
     self.assertEqual(text_obj.get_text(), sample_text)
-    self.assertEqual(text_obj.get_alpha(), 0)  # 初期は非表示（alpha=0）になっている
+    self.assertEqual(text_obj.get_alpha(), 1)  # 初期は非表示（alpha=0）になっている
     self.assertEqual(text_obj.get_fontname(), sample_font)
 
     # 作成した画像を tests/out に保存（削除はしない）
