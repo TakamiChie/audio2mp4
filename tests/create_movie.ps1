bf5ab150fe31bd66caace7845a9ee5129ed3a04b
TakamiChie@Ardha <chie@onpu-tamago.net>
2025-03-02 11:22:41 +0900

    テストコード追加

--- tests/create_movie.ps1
+++ tests/create_movie.ps1
@@ -2,4 +2,7 @@ pipenv run python ./src/audio2mp4.py `
     ./tests/test.mp3 `
     --bg-image ./tests/test.png `
     --loop-count 3 `
-    --effect fade
+    --effect fade `
+    --title テストタイトル `
+    --subtitle テストサブタイトル `
+    --summary テストサマリー
